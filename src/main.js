34e1cd741098801ac198be665e1b9db35c7677e9
Shots243 <fawwazandsyafiq@gmail.com>
2020-04-29 10:01:27 +0100

    Create table

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+// Game colours.
+const COLOURS = [
+    "FF0000",  // Red
+    "FFFF00",  // Yellow
+    "0000FF",  // Blue
+    "00FF00",  // Green
+    "FF8800",  // Orange
+    "FF00FF",  // Magenta
+];
 
+// Gameplay field dimensions.
+const FIELD_LENGTH = 12;
+const FIELD_WIDTH = 16;
